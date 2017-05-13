edb0aba906fd1bac04d97ce7cd9020e44a129882
skippythemagnificent <george@cooke.estate>
2017-05-13 17:59:08 +0100

    Adding files

---
+++ ReactDemo/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactDemo.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReactDemo.Controllers
+{
+    public class HomeController : Controller
+	{
+		private static readonly IList<CommentModel> _comments;
+
+        /// <summary>
+        /// Hard coded initial comments set
+        /// </summary>
+		static HomeController()
+		{
+			_comments = new List<CommentModel>
+			{
+				new CommentModel
+				{
+					Id = 1,
+					Author = "Daniel Lo Nigro",
+					Text = "Hello ReactJS.NET World!"
+				},
+				new CommentModel
+				{
+					Id = 2,
+					Author = "Pete Hunt",
+					Text = "This is one comment"
+				},
+				new CommentModel
+				{
+					Id = 3,
+					Author = "Jordan Walke",
+					Text = "This is *another* comment"
+				},
+			};
+		}
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Fetch the comments data 
+        /// </summary>
+        /// <returns>The comments as a JSON dataset</returns>
+		[Route("comments")]
+		[ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+		public ActionResult Comments()
+		{
+			return Json(_comments);
+		}
+
+        /// <summary>
+        /// Submits a new comment
+        /// </summary>
+        /// <returns>Success message</returns>
+        /// <param name="comment">The comment to be submitted</param>
+		[Route("comments/new")]
+		[HttpPost]
+		public ActionResult AddComment(CommentModel comment)
+		{
+			// Create a fake ID for this comment
+			comment.Id = _comments.Count + 1;
+			_comments.Add(comment);
+			return Content("Success :)");
+		}
+    }
+}
---
+++ ReactDemo/wwwroot/js/tutorial.jsx
@@ -0,0 +1,139 @@
+﻿// Built using https://reactjs.net/getting-started/tutorial.html
+
+// CommentBox
+// Contains the whole of the comments functionality and DOM
+// Rendered in ReactDOM.render
+var CommentBox = React.createClass({
+  // AJAX Data Request
+  loadCommentsFromServer: function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', this.props.url, true);
+    xhr.onload = function() {
+      var data = JSON.parse(xhr.responseText);
+      this.setState({ data: data });
+    }.bind(this);
+    xhr.send();
+  },
+  // Data Submit
+  handleCommentSubmit: function(comment) {
+    var data = new FormData();
+    data.append('author', comment.author);
+    data.append('text', comment.text);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', this.props.submitUrl, true);
+    xhr.onload = function() {
+      this.loadCommentsFromServer();
+    }.bind(this);
+    xhr.send(data);
+  },
+  // Fires when the page first loads
+  getInitialState: function() {
+    return {data: []};
+  },
+  // Fires once the page has loaded at the point where data is fetched
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    window.setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  // Markup
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+      </div>
+    );
+  }
+});
+
+// CommentList
+// Renders a list of Comments
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+/// CommentForm
+/// Lets the user enter a name and a comment, and submits it to the server
+/// Note that the submit function is passed back to the parent as an event of the onCommentSubmit property
+var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    this.setState({author: '', text: ''});
+  },
+  render: function() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+/// Comment
+/// Renders an individual comment
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+/// Where the magic happens
+ReactDOM.render(
+  <CommentBox url="/comments" submitUrl="/comments/new" pollInterval={2000} />,
+  document.getElementById('content')
+);
